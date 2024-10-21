7859636ed48252b69b7a1dacc7a40eba13717a6e
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-10-21 18:31:12 +0200

    feat cambiado alertas a español y bien registro y login

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -8,7 +8,9 @@ const { registerValidation } = require('../../middleware/registerValidation')
 const register = async (req, res) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() })
+    return res.status(400).json({
+      errors: errors.array()
+    })
   }
 
   const { username, email, password } = req.body
